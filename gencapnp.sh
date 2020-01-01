51bbce5ac7af0f25cf0c893dd73cf75ab7b3c2e8
Ian Denhardt <ian@zenhack.net>
2019-12-31 20:08:19 -0500

    Fix go.mod directive & add comment re: -I paths for capnp.
    The first of these is more important; this is now not pointing at a path
    on my local machine.

--- gencapnp.sh
+++ gencapnp.sh
@@ -1,3 +1,6 @@
+# You will likely want to adjust the paths below to point to the correct
+# directories for both the sanstorm capnproto files and go-capnproto2;
+# these are hardcoded for @zenhack's machine right now :/
 capnp compile -ogo \
 	-I /home/isd/src/pub/go.sandstorm/capnp/ \
 	-I /home/isd/src/foreign/go-capnproto2/std/ \
